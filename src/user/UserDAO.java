ee8dbf00a87befbadbae7b4b5e6eda7f6405fdf1
Kong ji han <wisejohn950330@gmail.com>
2022-03-23 23:41:14 +0900

    게시글 보기, 수정 및 삭제 기능 구현

--- src/user/UserDAO.java
+++ src/user/UserDAO.java
@@ -17,7 +17,7 @@ public class UserDAO {
 			String dbURL = "jdbc:mysql://localhost:3306/BBS";
 			String dbID = "root";
 			String dbPassword = "kjh950330";
-			Class.forName("com.mysql.jdbc.Driver");
+			Class.forName("com.mysql.cj.jdbc.Driver");
 			conn = DriverManager.getConnection(dbURL, dbID, dbPassword);
 		} catch (Exception e) {
 			e.printStackTrace();
